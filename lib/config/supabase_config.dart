8dec5094790b012bd0333b3bcc2ec40ad919bd61
kishorbarki123 <kishorbarki6@gmail.com>
2026-07-28 15:10:03 +0530

    Update Flutter portfolio app

--- lib/config/supabase_config.dart
+++ lib/config/supabase_config.dart
@@ -1,9 +1,9 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseConfig {
-  static const String url = 'https://ochajhysuwyixtkvymno.supabase.co';
+  static const String url = 'https://tkuwylszkymvdcvcnrwu.supabase.co';
   static const String anonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jaGFqaHlzdXd5aXh0a3Z5bW5vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUwMzYzMjYsImV4cCI6MjEwMDYxMjMyNn0.06O_O5C4rfherz054wCU-RE4li79bjGKaO9wuOBsbks';
+      'sb_publishable_-lKqWasq6b9czKxYwlN3Uw_YJ4FINj6';
 
   static Future<void> initialize() async {
     if (url.isEmpty || anonKey.isEmpty) {
